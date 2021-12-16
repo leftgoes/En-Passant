07278326bec56176461ced2051c0f1b22969a474
Ha Jong Kim <93378425+leftgoes@users.noreply.github.com>
2021-12-16 15:01:40 +0100

    Create gif.py

---
+++ gif.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import pyautogui
+import time
+
+
+class GIF:
+    __slots__ = ('folder', 'region')
+
+    def __init__(self, folder: str, region: tuple[int, int, int, int] = (460, 60, 975, 975)):
+        self.folder = folder
+        self.region = region
+
+    def test(self):
+        cv2.imshow('chess', cv2.cvtColor(np.array(pyautogui.screenshot(region=(460, 60, 975, 975))), cv2.COLOR_RGB2BGR))
+        cv2.waitKey(0)
+
+    def gif(self, n: int = 60, delay: float = 2):
+        time.sleep(delay)
+        print('starting capture')
+
+        for i in range(n):
+            s = cv2.cvtColor(np.array(pyautogui.screenshot(region=(460, 60, 975, 975))), cv2.COLOR_RGB2BGR)
+            cv2.imwrite(f'{self.folder}\\frm{n - i - 1}.png', s)
+
+        print('end capture')
+
+
+if __name__ == '__main__':
+    gif = GIF(r'C:\Python\En Passant\images')
+    gif.gif()
