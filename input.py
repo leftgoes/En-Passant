7c09063a5c8c931372a8ee5f5bb1e391c3869dde
Ha Jong Kim <93378425+leftgoes@users.noreply.github.com>
2021-12-27 01:56:56 +0100

    all constant variables
    - put COLORS and NUMBERS in input.py
    - LOG_HEADERS: headers in log file
    - PLT_STYLE: style of plt-figures

---
+++ input.py
@@ -0,0 +1,4 @@
+COLORS = ['#f0d9b5', '#b58863', '#b57563', '#b5534b', '#ca6565', '#ff9f9f']
+LOG_HEADERS = ['datetime', 'month', 'elapsed', 'bytes', 'games', 'detected', 'games', 'total', 'king', 'qbishop', 'qknight', 'kcastle', 'qcastle', 'enpassant']
+NUMBERS = ['king', 'qbishop', 'qknight', 'kcastle', 'qcastle', 'enpassant']
+PLT_STYLE = 'dark_background'
